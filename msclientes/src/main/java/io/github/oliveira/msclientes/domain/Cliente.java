c992b04b778242482b126033d273cbe750e34a8c
Ailton da Costa <ailtondacosta-91@hotmail.com>
2023-02-09 00:27:03 -0300

    create Cliente Service

---
+++ msclientes/src/main/java/io/github/oliveira/msclientes/domain/Cliente.java
@@ -0,0 +1,29 @@
+package io.github.oliveira.msclientes.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String cpf;
+    @Column
+    private String nome;
+    @Column
+    private Integer idade;
+
+    public Cliente(Long id, String cpf, String nome, Integer idade) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
